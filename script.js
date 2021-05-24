187dbf21fbe5d0f48edfd641188d6f2b1afff0ed
Abdul Rehman Khalil <abdulrehmankhalil@Abduls-MacBook-Air.local>
2021-05-24 15:50:17 -0400

    local storgae save working

--- script.js
+++ script.js
@@ -7,53 +7,38 @@ let tasks = {
     five: [],
     six: [],
     seven: [],
+    eight: [],
 };
 let getDate = moment().format ("Do MMMM YYYY h:mm:ss a");
 let getHour = parseInt(moment().format ("HH"));
-let taskTimeStatusSaveBtn;
 let taskIdCounter = 0;
 let taskArrPositionConverter;
-// data-task-Id is linked with taskIdCounter. every time we add a new task we will add taskCounterId ++
-// setAttribute("data-task-id", taskIdCounter);The setAttribute() method can be used to add or update any attribute on an HTML element, but the only attribute we need for now is data-task-id, which we set to the current value of taskIdCounter.
 
 // Jquery methods
 $(document).ready(function(){
 // this gets the date from the variable and displays it
 $("#todaysDateEl").text(getDate);
-// select all span tags
 
+// select all span tags
 $("span").each(function(index) {
-  // console.log($(this).text());
   // this adds 9 to the index of the span tags
-  let indexTimeConverter = parseInt(index) + 9;
-  // console.log("this is a", typeof indexTimeConverter, indexTimeConverter);
-  // console.log("this is a number", typeof getHour, getHour);
+let indexTimeConverter = parseInt(index) + 9;
   if (indexTimeConverter === getHour) {
     $(this).closest("div").css({"border": "3px solid green"})    
  }
   else if ( indexTimeConverter < getHour) {
-    // console.log("not found")
     $(this).closest("div").css({"border": "3px solid blue"}) 
   }
   else if ( indexTimeConverter > getHour) {
-    // console.log("not found")
     $(this).closest("div").css({"border": "3px solid orangered"}) 
   }
-
 })
 
-// When that happens, we need to collect a few pieces of data: the 
-// current value of the element, the parent element's ID, and the element's position in the list.
 $(".form-control").on("click", function () { 
-  // $(".form-control").on("blur", "textarea",function () {  
-  // } );
    // get current text - "this" is currently set to the input tag.
-   console.log($(this));
-  let currentText = $(this).text().trim();  
-  console.log(currentText);
-  
-  // // this converts the time into an intiger which can be linked to the array.
-  // let taskTimeStatus = parseInt($("textarea").siblings(".input-group-prepend").find("span").text());
+  let currentText = $(this).val().trim();    
+  // // finds the corresponding time hour of where the button is clicked and this converts the time into an intiger which can be linked to the array.
+  let taskTimeStatus = parseInt($(this).siblings(".input-group-prepend").find("span").text());
   // console.log(taskTimeStatus);
   let taskArrPositionNumber = taskTimeStatus - 9;
   if  (taskArrPositionNumber === 0) {
@@ -68,36 +53,47 @@ $(".form-control").on("click", function () {
     taskArrPositionConverter = "four";
   }if (taskArrPositionNumber === 5) {
     taskArrPositionConverter = "five";
+  }if (taskArrPositionNumber === 6) {
+    taskArrPositionConverter = "six";
+  }if (taskArrPositionNumber === 7) {
+    taskArrPositionConverter = "seven";
+  }if (taskArrPositionNumber === 8) {
+    taskArrPositionConverter = "eight";
   }
-  
-  console.log(tasks[taskArrPositionConverter]);
-  tasks[taskArrPositionConverter].push(text);
+  tasks[taskArrPositionConverter].push(currentText);
   taskIdCounter++;
 }) 
 
 $(".save-button").on("click", function () {
-  taskTimeStatusSaveBtn = parseInt($(this).siblings(".input-group-prepend").find("span").text())
   localStorage.setItem("tasks", JSON.stringify(tasks));
 })
+
+var loadTasks = function() {
+  tasks = JSON.parse(localStorage.getItem("tasks"));
+
+  // if nothing in localStorage, create a new object to track all task status arrays
+  if (!tasks) {
+    tasks = {
+      zero: [],
+      one: [],
+      two: [],
+      three: [],
+      four: [],
+      five: [],
+      six: [],
+      seven: [],
+      eight: [],
+  };
+  }
+
+  // loop over object properties
+  $.each(tasks, function(list, arr) {
+    console.log(list, arr);
+    // then loop over sub-array
+    arr.forEach(function(task) {
+      createTask(task.text);
+    });
+  });
+};
+
 });
-
-
-
-
-
-
-
-
-
-
-// this generates a ptag on click on that specific 
-// $("<p>").appendTo(this);
-// // let clickedTextArea = $(this);
-// let dynamicPTag = $(this).find("p").addClass("form-control").attr("data-task-id", taskIdCounter).text(currentText);
-// // create a new text area input
-// let textInput = $("<textarea>").addClass("form-control").attr("data-task-id", taskIdCounter).text(dynamicPTag.text());
-// console.log(textInput.val());
-
-// // this replaces the div with the textinput
-// $(this).replaceWith(textInput);
-// textInput.trigger("focus");
